759f87cc61690bae6e10bf540b2c5c3aaff883cd
Carlos Gomes <dev.shinsei@gmail.com>
2022-09-16 18:50:17 -0300

    bug fixing

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://cg-rocketmovies-api.herokuapp.com/",
+  baseURL: "https://cg-rocketmovies-api.herokuapp.com",
 });
